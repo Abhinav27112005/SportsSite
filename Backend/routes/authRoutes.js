2428d8405842f835a51c9adce26561d88bff3485
Abhinav <abhinavkumarjha763@gmail.com>
2025-06-17 17:52:27 +0530

    importissue

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { signup, login } from '../controllers/authController.js';
+import { signup, login, refreshToken } from '../controllers/authController.js';
 import { verifyToken } from '../middleware/auth.js';
 
 const router = express.Router();
